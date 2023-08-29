ed57c0d7ed3c55d9737888b38617df042dcce9a2
mdEstrada314 <mdelprograma@gmail.com>
2023-08-29 07:43:51 -0500

    feat: :sparkles: asicronas

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs')
 
 const textInt = fs.readFileSync('./txt/input.txt','utf-8')
@@ -10,3 +11,21 @@ fs.writeFileSync('./txt/output.txt', textOut)
 const textInt2 = fs.readFileSync('./txt/output.txt','utf-8')
 console.log(textInt2);
 console.log('File written');
+
+
+
+
+fs.readFile('./txt/start.txt','utf-8', (err,data1)=>{
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
+                console.log('no entendi');
+
+            })
+        })
+    })
+})
+
+console.log('pailas');
